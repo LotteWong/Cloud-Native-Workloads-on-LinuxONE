2f87523afc7a42c4ed681741add52f586a001252
LotteWong <LotteWong21@gmail.com>
2019-05-26 22:41:22 +0800

    test query, deposit and withdraw

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -67,7 +67,7 @@ angular.module('todoController', [])
 					console.log(msg);
 					
 					for(var item in data) {
-						console.log("item id: " + item);
+						console.log("item id: " + item["id"]);
 						console.log("selected id: " + $scope.selected);
 						if(item["_id"] == $scope.selected) {
 							console.log("Query " + item["balance"] + " yuan");

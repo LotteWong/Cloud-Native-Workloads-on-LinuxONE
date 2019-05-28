2d8e636f633ce174efda8c40a06d57dd97e94b26
LotteWong <LotteWong21@gmail.com>
2019-05-28 22:24:16 +0800

    test the backend

--- files/mean-docker/express-server/public/js/core.js
+++ files/mean-docker/express-server/public/js/core.js
@@ -4,6 +4,6 @@
 // 'financeService'
 
 // angular.module('meanTodo', ['todoController', 'todoService']);
-angular.module('meanTodo', ['todoController', 'customerService']);
-// angular.module('meanTodo', ['todoController', 'todoService', 'CustomerService']);
+// angular.module('meanTodo', ['todoController', 'customerService']);
+angular.module('meanTodo', ['todoController', 'todoService', 'customerService']);
 

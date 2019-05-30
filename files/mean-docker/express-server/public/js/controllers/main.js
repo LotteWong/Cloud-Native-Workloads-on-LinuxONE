9097c1deacf5eecfbe0e594b378fce821dee801b
zhao201730685110 <924265424zzr@sina.com>
2019-05-30 19:03:57 +0800

    commit

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -486,7 +486,7 @@ angular.module('todoController', [])
 			$scope.transactionData.to = $scope.currCustomer.username;
 			$scope.transactionData.time = dateTime.toLocaleString();
 			$scope.transactionData.amount=parseFloat($scope.financeData.amount);
-			console.log("投资金额："+$scope.$financeData.amount);
+			console.log("投资金额："+$scope.financeData.amount);
 			$scope.financeData.amount="";
 
 			var msg = JSON.stringify($scope.transactionData);
